60f09613d71b21219023cadde447389665fa2709
inamsaif0 <inamsaif586@gmail.com>
2024-01-04 22:41:59 -0800

    lalaa

--- component/AudioTable.js
+++ component/AudioTable.js
@@ -25,10 +25,22 @@ export default function BasicTable() {
     const router = useRouter()
     async function getData() {
         const email = Cookies.get('userEmail');
+        const role = Cookies.get('userRole');
 
-        await fetch(`/api/audio/getAudio?email=${email}`)
+        if (role === 'teacher') {
+            let data = await fetch(`/api/audio/getAudio?email=${email}`)
+                .then((response) => response.json())
+                .then((data) => setValue(data))
+
+            return data;
+        }
+        else {
+            let data = await fetch(`/api/audio/getAllAudios`)
             .then((response) => response.json())
             .then((data) => setValue(data))
+
+        return data;
+        }
     }
 
     // const handleEdit = (additionalProp) => {
@@ -44,7 +56,7 @@ export default function BasicTable() {
             if (response.data.success) {
                 // getData();
                 console.log(`Audio with studentId ${id} deleted successfully.`);
-                
+
 
 
             } else {
@@ -76,155 +88,167 @@ export default function BasicTable() {
 
     async function getData() {
         const email = Cookies.get('userEmail');
+        const role = Cookies.get('userRole');
 
-        await fetch(`/api/audio/getAudio?email=${email}`)
+        if (role === 'teacher') {
+            let data = await fetch(`/api/audio/getAudio?email=${email}`)
+                .then((response) => response.json())
+                .then((data) => setValue(data))
+
+            return data;
+        }
+        else {
+            let data = await fetch(`/api/audio/getAllAudios`)
             .then((response) => response.json())
             .then((data) => setValue(data))
-    }
 
-    async function handleActive(index) {
-        console.log(value.data[index])
-        const response = await axios.post('/api/active', {
-            status: !(value.data[index].status),
-            studentId: value.data[index].studentId
-        }).then({
-
-        })
-        // console.log(response)
-        getData()
-    }
-    const handleUpdateStatus = async (id, status) => {
-        //   setMessage('Rejected');
-
-        try {
-            // Replace '' with your actual backend URL
-            const response = await axios.post('/api/audio/editAudio', {
-                _id: id,
-                status: status
-            });
-            getData()
-            console.log(response.data); // Log the response from the server
-        } catch (error) {
-            console.error('Error updating status:', error);
+        return data;
         }
-    };
+    }
 
-    const handleCancelClick = () => {
-        setMessage('Rejected');
+        async function handleActive(index) {
+            console.log(value.data[index])
+            const response = await axios.post('/api/active', {
+                status: !(value.data[index].status),
+                studentId: value.data[index].studentId
+            }).then({
 
-    };
+            })
+            // console.log(response)
+            getData()
+        }
+        const handleUpdateStatus = async (id, status) => {
+            //   setMessage('Rejected');
 
-    const handleCheckCircleClick = () => {
-        setMessage('Accepted');
-    };
+            try {
+                // Replace '' with your actual backend URL
+                const response = await axios.post('/api/audio/editAudio', {
+                    _id: id,
+                    status: status
+                });
+                getData()
+                console.log(response.data); // Log the response from the server
+            } catch (error) {
+                console.error('Error updating status:', error);
+            }
+        };
 
-    const handleDeleteClick = () => {
-        setMessage('Delete clicked!');
-    };
+        const handleCancelClick = () => {
+            setMessage('Rejected');
 
-    useEffect(() => {
+        };
+
+        const handleCheckCircleClick = () => {
+            setMessage('Accepted');
+        };
+
+        const handleDeleteClick = () => {
+            setMessage('Delete clicked!');
+        };
+
+        useEffect(() => {
 
 
-        getData()
-    }, [])
+            getData()
+        }, [])
 
 
-    var i = 0;
-    return (
+        var i = 0;
+        return (
 
-        <Grid container justifyContent="center" spacing={2}>
-            <Grid item xs={8} sm={8} md={8} lg={10} xl={12}>
-                <TableContainer component={Paper} sx={{ borderRadius: '15px', overflowX: 'auto', minWidth: '100%', margin: '20px', marginLeft: 'auto', marginRight: 'auto', mr: { md: '4rem', sm: '4rem' } }}>
-                    <Table aria-label="simple table" stickyHeader={true}>
-                        <TableHead>
-                            <TableRow>
-                                <TableCell><b>Teacher No.</b></TableCell>
-                                <TableCell align='left'><b>Audio Link</b></TableCell>
-                                <TableCell align="left"><b>TeacherID&nbsp;(email)</b></TableCell>
-                                <TableCell align="left"><b>Student</b></TableCell>
-                                {/* <TableCell align="center"><b>Status</b></TableCell> */}
-                                <TableCell align="center"><b>Options</b></TableCell>
-                                <TableCell align="center"><b>Delete</b></TableCell>
+            <Grid container justifyContent="center" spacing={2}>
+                <Grid item xs={8} sm={8} md={8} lg={10} xl={12}>
+                    <TableContainer component={Paper} sx={{ borderRadius: '15px', overflowX: 'auto', minWidth: '100%', margin: '20px', marginLeft: 'auto', marginRight: 'auto', mr: { md: '4rem', sm: '4rem' } }}>
+                        <Table aria-label="simple table" stickyHeader={true}>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell><b>Teacher No.</b></TableCell>
+                                    <TableCell align='left'><b>Audio Link</b></TableCell>
+                                    <TableCell align="left"><b>TeacherID&nbsp;(email)</b></TableCell>
+                                    <TableCell align="left"><b>Student</b></TableCell>
+                                    {/* <TableCell align="center"><b>Status</b></TableCell> */}
+                                    <TableCell align="center"><b>Options</b></TableCell>
+                                    <TableCell align="center"><b>Delete</b></TableCell>
 
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {value?.result
-                                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                                .map((curElem, index) => {
-                                    const rowIndex = page * rowsPerPage + index + 1;
-                                    return (
-                                        <TableRow key={index} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                                            <TableCell component="th" scope="curElement" align='center'>
-                                                {rowIndex}
-                                            </TableCell>
-                                            <TableCell align="left" sx={{ fontFamily: "inherit" }}>
-                                                <a style={{ color: '#5c0931' }} href={curElem.audioLink}>
-                                                    {/* {curElem.filename} */}
-                                                    Audio
-                                                </a>
-                                            </TableCell>
-                                            <TableCell align="left" sx={{ fontFamily: 'inherit' }}>{curElem.teacherEmail}</TableCell>
-                                            <TableCell align="left" sx={{ fontFamily: "inherit" }}>{curElem.ownerEmail}</TableCell>
-                                            {/* <TableCell align="left" sx={{ fontFamily: "inherit" }}>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {value?.result
+                                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                                    .map((curElem, index) => {
+                                        const rowIndex = page * rowsPerPage + index + 1;
+                                        return (
+                                            <TableRow key={index} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                                                <TableCell component="th" scope="curElement" align='center'>
+                                                    {rowIndex}
+                                                </TableCell>
+                                                <TableCell align="left" sx={{ fontFamily: "inherit" }}>
+                                                    <a style={{ color: '#5c0931' }} href={curElem.audioLink}>
+                                                        {/* {curElem.filename} */}
+                                                        Audio
+                                                    </a>
+                                                </TableCell>
+                                                <TableCell align="left" sx={{ fontFamily: 'inherit' }}>{curElem.teacherEmail}</TableCell>
+                                                <TableCell align="left" sx={{ fontFamily: "inherit" }}>{curElem.ownerEmail}</TableCell>
+                                                {/* <TableCell align="left" sx={{ fontFamily: "inherit" }}>
                         <Button disabled={false} variant="outlined" style={{ color: "#5c0931", borderColor: "#5c0931" }} onClick={() => handleActive(index)}>
                           {curElem.status ? "Active" : "Inactive"}
                         </Button>
                       </TableCell> */}
-                                            {/* <TableCell align="left" sx={{ fontFamily: "inherit" }}>
+                                                {/* <TableCell align="left" sx={{ fontFamily: "inherit" }}>
                         <Stack flexDirection='row'>
                           <Button onClick={() => handleEdit(curElem.email)}>
                             <EditIcon sx={{ color: '#5c0931' }} />
                           </Button>
                         </Stack>
                       </TableCell> */}
-                                            <TableCell align="left" sx={{ fontFamily: 'inherit' }}>
-                                                <Stack flexDirection="row" justifyContent="center">
-                                                    {curElem.status ? <b>{curElem.status}</b> :
-                                                        <>
-                                                            <Button onClick={()=>handleUpdateStatus(curElem._id, status = "Rejected")}>
-                                                                <CancelIcon sx={{ color: '#5c0931' }} />
-                                                            </Button>
-                                                            <Button onClick={()=>handleUpdateStatus(curElem._id, status = "Accepted")}>
-                                                                {/* {message ? message : } */}
-                                                                <CheckCircleIcon sx={{ color: '#5c0931' }} />
-                                                            </Button>
-                                                        </>}
+                                                <TableCell align="left" sx={{ fontFamily: 'inherit' }}>
+                                                    <Stack flexDirection="row" justifyContent="center">
+                                                        {curElem.status ? <b>{curElem.status}</b> :
+                                                            <>
+                                                                <Button onClick={() => handleUpdateStatus(curElem._id, status = "Rejected")}>
+                                                                    <CancelIcon sx={{ color: '#5c0931' }} />
+                                                                </Button>
+                                                                <Button onClick={() => handleUpdateStatus(curElem._id, status = "Accepted")}>
+                                                                    {/* {message ? message : } */}
+                                                                    <CheckCircleIcon sx={{ color: '#5c0931' }} />
+                                                                </Button>
+                                                            </>}
 
-                                                </Stack>
-                                            </TableCell>
-                                            <TableCell align="left" sx={{ fontFamily: 'inherit' }}>
-                                                <Stack flexDirection="row" justifyContent="center">
-                                                    <Button onClick={()=> {
-                                                        deleteAudio(curElem._id)
-                                                    }}>
-                                                        <DeleteIcon sx={{ color: '#5c0931' }} />
-                                                    </Button>
-                                                </Stack>
-                                            </TableCell>
-                                        </TableRow>
-                                    );
-                                })}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
+                                                    </Stack>
+                                                </TableCell>
+                                                <TableCell align="left" sx={{ fontFamily: 'inherit' }}>
+                                                    <Stack flexDirection="row" justifyContent="center">
+                                                        <Button onClick={() => {
+                                                            deleteAudio(curElem._id)
+                                                        }}>
+                                                            <DeleteIcon sx={{ color: '#5c0931' }} />
+                                                        </Button>
+                                                    </Stack>
+                                                </TableCell>
+                                            </TableRow>
+                                        );
+                                    })}
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Grid>
+                <Grid item xs={8} sm={8} md={8} lg={10} xl={12}>
+                    <TablePagination
+                        rowsPerPageOptions={[5, 25, 100]}
+                        component="div"
+                        count={value && value.result ? value.result.length : 0}
+                        rowsPerPage={rowsPerPage}
+                        page={page}
+                        onPageChange={handleChangePage}
+                        onRowsPerPageChange={handleChangeRowsPerPage}
+                    />
+                </Grid>
             </Grid>
-            <Grid item xs={8} sm={8} md={8} lg={10} xl={12}>
-                <TablePagination
-                    rowsPerPageOptions={[5, 25, 100]}
-                    component="div"
-                    count={value && value.result ? value.result.length : 0}
-                    rowsPerPage={rowsPerPage}
-                    page={page}
-                    onPageChange={handleChangePage}
-                    onRowsPerPageChange={handleChangeRowsPerPage}
-                />
-            </Grid>
-        </Grid>
 
 
-    );
-}
+        );
+    }
 // BasicTable.getInitialProps = async ctx => {
 //   try {
 //     const res = await axios.get('/api/userList');
---
+++ pages/api/audio/getAllAudios.js
@@ -0,0 +1,61 @@
+// app.get('/audio',async(req,res)=>{
+    // const { email } = req.query;
+    // const bucketName = 'otp-mobile';
+    // const prefix = "otp-audio/"+email+"/";
+  
+    // try{
+    //   const params={
+    //     Bucket:bucketName,
+    //     Delimiter: '/',
+    //     Prefix: prefix
+    //   }
+    //   const data = await s3.listObjects(params).promise();
+    //   const fileKeys = data.Contents.map(obj => obj.Key);
+    //   res.json(fileKeys);
+    // }
+    // catch (error) {
+    //   console.error('Error retrieving files:', error);
+    //   res.status(500).json({ error: 'Failed to retrieve files' });
+    // }
+  
+//   })
+import connect from '../../../lib/mongodb'
+import Teachers from '../../../model/teachers';
+import audioFiles from '../../../model/audioFiles';
+connect();
+
+const teacherLists = async (req, res) => {
+
+    let data = req
+    // const { email } = req.query;
+    // const bucketName = 'otp-mobile';
+    // const prefix = "otp-audio/"+email+"/";
+    // const query = { teacherEmail: email };
+
+    // const result = await collection.find(query).toArray();
+    console.log(data,'sssssssssssss')
+    // return data;
+    // res.status(200).json({result})
+    try{
+
+      // let data = req
+      // const { email } = req.query;
+      // const bucketName = 'otp-mobile';
+      // const prefix = "otp-audio/"+email+"/";
+      // const query = { teacherEmail: email };
+  
+      const result = await audioFiles.find({});
+      
+      console.log(data,'sssssssssssss')
+      // return data;
+      res.status(200).json({result})
+    }
+    catch (error) {
+      console.error('Error retrieving files:', error);
+      res.status(500).json({ error: 'Failed to retrieve files' });
+    }
+            
+    }
+
+
+export default teacherLists;
